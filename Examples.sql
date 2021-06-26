186ca501a1ca0572cdff4cdfd63820a70c065e54
Petr Jíša <petr.jisa1406@gmail.com>
2021-06-26 14:06:18 +0200

    File after final check

--- Examples.sql
+++ Examples.sql
@@ -1,4 +1,4 @@
--- Example 1: Query for mining all data FROM summer 2020  ANDwinter 2020/2021
+-- Example 1: Query for mining all data from summer 2020 and winter 2020/2021
 SELECT
 	*
 FROM t_petr_jíša_project_sql_final
@@ -7,8 +7,8 @@ WHERE
 	AND date > '2020-05-31'
 ORDER BY date;
 
--- Example 2: Query for mining only the epidemiological data, WHERE performed tests are accessible
--- Adding the ratio of positive tests, confirmed on million  ANDdeaths on milion
+-- Example 2: Query for mining only the epidemiological data, where performed tests are accessible
+-- Adding the ratio of positive tests, confirmed on million and deaths on milion
 SELECT
 	date,
 	country,
@@ -39,9 +39,9 @@ ORDER BY date;
 -- THESE PROBLEMATIC DATA COME FROM THE DATABASE, NOT FROM A WRONG PROJECT SOLUTION
 -- ONCE THE PROBLEM IS FOUND, IT ALSO CAN BE SOLVED... 
 
--- Example 4 (WHERE the problem was found): Query for mining the maximum daily percentage of positive tests for countries where Christianity religion dominates over the other religions
--- This table also shows that in many CASEs, there is a problem with compatibility of the data regarding confirmed  ANDperformed tests
--- For 14 countries, there is maximum of the ratio between positive tests  ANDperformed tests over 100 %
+-- Example 4 (where the problem was found): Query for mining the maximum daily percentage of positive tests for countries where Christianity religion dominates over the other religions
+-- This table also shows that in many CASEs, there is a problem with compatibility of the data regarding confirmed and performed tests
+-- For 14 countries, there is maximum of the ratio between positive tests and performed tests over 100 %
 -- Even the values between 90 - 100 % which are present for 4 countries give a warning signal 
 SELECT
 	country,
@@ -62,8 +62,8 @@ WHERE
 	ORDER BY max(confirmed*100/tests_performed) DESC;
 
 
--- Example 5: Query to find the countries, WHERE maximum percentage of positive tests exceeds 100 %
--- It shows all problematic countries WHERE the relation between positive  ANDperformed tests may be problematic
+-- Example 5: Query to find the countries, where maximum percentage of positive tests exceeds 100 %
+-- It shows all problematic countries where the relation between positive and performed tests may be problematic
 -- The amount of such countries is 19
 -- The data for these countries should be explored more in detail
 SELECT
@@ -75,8 +75,8 @@ GROUP BY country
 having max(confirmed*100/tests_performed) > 100
 ORDER BY max(confirmed*100/tests_performed) DESC;
 
--- Example 6: What is the amount of data records for each country, WHERE the ratio between confirmed  ANDperformed tests is above 100 % ??
--- After this query execution, we can see 82 amount of records in Peru (really problematic)  AND17 in Mexico (still not good)
+-- Example 6: What is the amount of data records for each country, where the ratio between confirmed  and performed tests is above 100 % ??
+-- After this query execution, we can see 82 amount of records in Peru (really problematic)  and 17 in Mexico (still not good)
 -- Amount of these CASEs for the other countries is up to 5 (only exceptions)
 -- Now we can decide what to do with data FROM these countries, where there are big problems, where there are only exceptions, etc.
 SELECT
